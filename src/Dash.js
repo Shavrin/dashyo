aa7dc3a07d91873409c317fbce54085d63d34c44
Kacper <kacper.olek@gmail.com>
2019-12-08 23:35:41 +0100

    Cleanup and Dash component with basic style

---
+++ src/Dash.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const styles = {
+    height: '100vh',
+    width: '100vw',
+    backgroundColor: 'black',
+    border: '5px solid white'
+};
+
+function Dash(){
+    return (
+        <main style={styles}></main>
+    );
+}
+
+export default Dash;
