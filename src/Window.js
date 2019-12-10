6a13a46d175488b873905f5e78f67997c22eb918
Kacper <kacper.olek@gmail.com>
2019-12-10 23:41:22 +0100

    draggable windows and displaying widget

--- src/Window.js
+++ src/Window.js
@@ -1,6 +1,41 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
+import displace from 'displacejs';
+import { relative } from 'path';
 
 export default function Window() {
     const [title, setTitle] = useState('unnamed window');
-    return <div>{title}</div>;
+    const [size, setSize] = useState({width: 250, height: 200});
+    const [position, setPosition] = useState({x: 50, y: 50});
+
+    const windowStyle = {
+        backgroundColor: 'grey',
+        height: size.height,
+        width: size.width,
+        position: 'absolute',
+        userSelect: 'none',
+        cursor: 'pointer'
+    }
+
+    const iframeStyle = {
+        height: '95%',
+        width: '100%',
+        position: 'relative',
+        top: '5%'
+    }
+
+    const windowElement = useRef(null);
+    useEffect(()=>{
+        displace(windowElement.current, {
+            constrain: true,
+            onMouseDown: () => {
+                windowElement.current.style['pointer-events'] = 'none';
+
+            },
+            onMouseUp: () => {
+                windowElement.current.style['pointer-events'] = 'auto';
+        }});
+    });
+    return (<div ref={windowElement} style={windowStyle}>
+        <iframe title="window" style={iframeStyle} src="./widgets/timer.html"></iframe>
+    </div>);
 }
