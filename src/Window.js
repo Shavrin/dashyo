7d4c4fa1b34b70f39ca390be924422e54409fdcb
Kacper Olek <kacper.olek@gmail.com>
2019-12-12 19:18:20 +0100

    topbar and styling

--- src/Window.js
+++ src/Window.js
@@ -1,28 +1,52 @@
 import React, { useState, useRef, useEffect } from 'react'
 import displace from 'displacejs';
+import styled from 'styled-components';
+
 
 export default function Window() {
     const [title, setTitle] = useState('unnamed window');
     const [size, setSize] = useState({width: 250, height: 200});
     const [position, setPosition] = useState({x: 50, y: 50});
 
-    const windowStyle = {
-        backgroundColor: 'grey',
-        height: size.height,
-        width: size.width,
-        position: 'absolute',
-        userSelect: 'none',
-        cursor: 'pointer',
-        borderRadius: '10px',
-        border: '1px solid white'
-    }
 
-    const iframeStyle = {
-        height: '95%',
-        width: '100%',
-        position: 'relative',
-        top: '5%'
-    }
+    const WidgetTopBar = styled.div`
+        position: absolute;
+        width: 100%;
+        height: 20px;
+        top: -20px;
+        background-color: blue;
+        display:flex;
+        justify-content: space-between;
+        align-items: center;
+        border-radius: 10px 10px 0 0 ;
+    `;
+    const WidgetWindow = styled.div`
+        height: ${size.height + 10 + 'px'};
+        width: ${size.width + 'px'};
+        position: absolute;
+        user-select: none;
+        cursor: pointer;
+        background-color: white;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        border-radius: 0 0 10px 10px;
+    `;
+
+
+    const WidgetTitle = styled.div`
+    `;
+    const WidgetCloseButton = styled.img`
+        height: 100%;
+    `;
+
+    const WidgetIframe = styled.iframe`
+        height: 95%;
+        width: 95%;
+        border: none;
+        border-radius: 10px;
+        background-color: grey;
+    `;
 
     const windowElement = useRef(null);
     useEffect(()=>{
@@ -36,7 +60,11 @@ export default function Window() {
                 windowElement.current.style['pointer-events'] = 'auto';
         }});
     });
-    return (<div ref={windowElement} style={windowStyle}>
-        <iframe title="window" style={iframeStyle} src="./widgets/timer.html"></iframe>
-    </div>);
+    return (<WidgetWindow ref={windowElement}>
+        <WidgetTopBar>
+            <WidgetTitle>{title}</WidgetTitle>
+            <WidgetCloseButton src='./svg/closeIcon.svg'></WidgetCloseButton>
+        </WidgetTopBar>
+        <WidgetIframe src="./widgets/timer.html"></WidgetIframe>
+    </WidgetWindow>);
 }
--- src/index.js
+++ src/index.js
@@ -17,7 +17,10 @@ store.dispatch( addWindow({
     id: store.getState().windows.length,
     title: 'basic window'
   }));
-
+  store.dispatch( addWindow({
+    id: store.getState().windows.length,
+    title: 'basic 2'
+  }));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
